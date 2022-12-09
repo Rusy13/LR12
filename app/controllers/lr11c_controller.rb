07150f498513123933658c253c5605e6e559858e
Rusy13 <rusugarov@gmail.com>
2022-12-09 12:49:22 +0300

    without tests

--- app/controllers/lr11c_controller.rb
+++ app/controllers/lr11c_controller.rb
@@ -1,15 +1,11 @@
 class Lr11cController < ApplicationController
     # before_action :check_num, only: :result
   
-  
     def input
     end
-
     def view
     end
-
     def show
       render xml: User.all.as_json.to_xml
     end
-
   end
